e501817d7755d808428ac69380c765060ebae1a7
Brwnlee <77315942+ValiantCraven27@users.noreply.github.com>
2021-03-10 18:56:03 -0500

    Add files via upload

---
+++ pNotify/fxmanifest.lua
@@ -0,0 +1,20 @@
+fx_version "bodacious"
+
+games {"rdr3"}
+rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
+
+ui_page "html/index.html"
+
+client_script "cl_notify.lua"
+
+export "SetQueueMax"
+export "SendNotification"
+
+files {
+    "html/index.html",
+    "html/pNotify.js",
+    "html/noty.js",
+    "html/noty.css",
+    "html/themes.css",
+    "html/sound-example.wav"
+}
